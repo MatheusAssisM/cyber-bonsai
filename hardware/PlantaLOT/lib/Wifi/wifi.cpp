58b51eb3d7cf2614438c6fa8b908cf42d90818c1
Matheus De Assis <65235458+MatheusAssisM@users.noreply.github.com>
2021-02-20 17:48:40 -0300

    add wifi settings

---
+++ hardware/PlantaLOT/lib/Wifi/wifi.cpp
@@ -0,0 +1,27 @@
+#include "eeprom.h"
+#include <ESP8266WiFi.h>
+#include "rgb.h"
+
+void VerificaConexao()
+{
+    String login = eepromReadlogin();
+    String senha = eepromReadsenha();
+    Serial.println("\n\nEEPROM SSID: " + (String)login + "   EEPROM SENHA: " + (String)senha);
+
+    //Imprime SSID e SENHA
+    if (login.length() > 0)
+    {
+        piscarLedWifi(7);
+        Serial.println("\nTentando conectar, aguarde...");
+        WiFi.begin(login, senha);
+    }
+
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        Serial.println("Conectado!");
+        }
+    else
+    {
+        Serial.print("\nFalha ao conectar, pressione o botão WPS no seu roteador ...");
+    }
+}
